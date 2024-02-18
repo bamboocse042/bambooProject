322bf2b84138a232dcd2b599239e8604e3d78ce9
Keshav <keshav@microsoft.com>
2024-02-18 21:56:30 +0530

    removed use of env for now

--- server/utils/otp.js
+++ server/utils/otp.js
@@ -9,15 +9,18 @@ const sendOTP = (email, OTP) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_SERVICE_USER,
-      pass: process.env.EMAIL_SERVICE_PASS,
+      // user: process.env.EMAIL_SERVICE_USER,
+      // pass: process.env.EMAIL_SERVICE_PASS,
+      user: "bamboo.cse042@gmail.com",
+      pass: "xfci deqt pdrb bbvb",
     },
   });
 
   console.log(" ==> ", process.env.EMAIL_SERVICE_USER, " ==> ", process.env.EMAIL_SERVICE_PASS, "<==");
 
   const mailOptions = {
-    from: process.env.EMAIL_SERVICE_USER,
+    // from: process.env.EMAIL_SERVICE_USER,
+    from: "bamboo.cse042@gmail.com",
     to: email,
     subject: "Your OTP",
     text: `Your OTP is: ${OTP}`,
